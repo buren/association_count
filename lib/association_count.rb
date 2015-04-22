657d731acf28f6078a26cb6bb1e4678e8cf30a6a
Albin Svensson <albin.svensson@trialbee.com>
2015-04-22 17:50:18 +0200

    Added ability to turn of distinct count as an option

--- lib/association_count.rb
+++ lib/association_count.rb
@@ -3,28 +3,30 @@ require 'active_record'
 
 ActiveRecord::Base.extend AssociationCount
 module AssociationCount
-  def association_count(counted_model)
-    proc do |base_model, counted_model|
+  def association_count(counted_model, distinct)
+    proc do |base_model, counted_model, distinct|
       table_name    = base_model.table_name
       counted_table = counted_model.table_name
       counted_name  = counted_table.singularize
-      
+      distinct_string  = distinct ? 'DISTINCT' : ''
       joins(counted_table.to_sym).
-      select("#{table_name}.*, COUNT(DISTINCT #{counted_table}.id) as #{counted_name}_count_raw").
+      select("#{table_name}.*, COUNT(#{distinct_string} #{counted_table}.id) as #{counted_name}_count_raw").
       group("#{table_name}.id")
-    end.call(self, counted_model)
+    end.call(self, counted_model, distinct)
   end
 
-  def can_count(model_name)
+  def can_count(model_name, opts = {})
     model_name = model_name.to_s
     raise 'No such reflection' unless reflection = reflections[model_name.to_s]
     singular_name = model_name.singularize
+
     define_method "#{singular_name}_count" do
       return send("#{singular_name}_count_raw") if self.respond_to?("#{singular_name}_count_raw")
       send(model_name).count
     end
+    distinct = true unless opts[:distinct] === false
     class_eval do
-      scope "include_#{singular_name}_count", -> { association_count(reflection.klass) }
+      scope "include_#{singular_name}_count", -> { association_count(reflection.klass, distinct) }
     end
   end
 end
