43862809bb8ce19747d1510778b7d6ffae491e6d
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-29 18:00:04 +0800

    Delete spec directory

--- spec/association_count_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe AssociationCount do
-  it 'has a version number' do
-    expect(AssociationCount::VERSION).not_to be nil
-  end
-end
--- spec/support/data.rb
+++
@@ -1,7 +0,0 @@
-Author.create(name: 'Jacob')
-author = Author.create(name: 'Albin')
-
-post = Post.create(text: "First post!", author: author)
-
-Answer.create(text: 'A1', post: post, author: author)
-Answer.create(text: 'A2', post: post, author: author)
